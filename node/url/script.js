eef30c55cef296be196bd34b5bab56ebc3fa6cef
imyogeshgaur <yogeshgaur1995@gmail.com>
2023-03-29 05:57:51 +0530

    Code of 29th March

---
+++ node/url/script.js
@@ -0,0 +1,28 @@
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host); //Output -> 'localhost:8080'
+console.log(q.pathname); //Output -> '/default.htm'
+console.log(q.search); //Output -> '?year=2017&month=february'
+
+var qdata = q.query; 
+console.log(qdata.month); //Output -> 'february'
+
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
